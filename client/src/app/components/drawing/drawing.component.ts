1b7a024a8cd86f2badf86eecf19deb4d96d24050
Kevin Lam <kevinlam@Kevins-MacBook-Pro.local>
2021-02-13 12:15:44 -0500

    canvas dash preview and resize respect the limits

--- client/src/app/components/drawing/drawing.component.ts
+++ client/src/app/components/drawing/drawing.component.ts
@@ -21,16 +21,20 @@ export class DrawingComponent implements AfterViewInit {
     @ViewChild('baseCanvas', { static: false }) baseCanvas: ElementRef<HTMLCanvasElement>;
     // On utilise ce canvas pour dessiner sans affecter le dessin final
     @ViewChild('previewCanvas', { static: false }) previewCanvas: ElementRef<HTMLCanvasElement>;
-    // @ViewChild('bottomAnchor', { static: false }) bottomAnchor: ElementRef<button>;
 
     private baseCtx: CanvasRenderingContext2D;
     private previewCtx: CanvasRenderingContext2D;
 
     private canvasSize: Vec2 = { x: DEFAULT_WIDTH, y: DEFAULT_HEIGHT };
+    private previouscanvasSize: Vec2 = { x: DEFAULT_WIDTH, y: DEFAULT_HEIGHT };
+
     private mouseDown: boolean = false;
     isCorner: boolean = false;
     isSide: boolean = false;
     isBottom: boolean = false;
+    bottomAnchorPosition: Vec2;
+    sideAnchorPosition: Vec2;
+    cornerAnchorPosition: Vec2;
 
     timeOutDuration: number = 170;
 
@@ -46,6 +50,9 @@ export class DrawingComponent implements AfterViewInit {
         this.shortcutKeyboardManager = keyboardManager;
         this.toolManager.currentToolChange.subscribe((value) => (this.currentTool = value));
         this.currentTool = this.toolManager.currentTool;
+        this.bottomAnchorPosition = { x: this.canvasSize.x / 2, y: this.canvasSize.y };
+        this.sideAnchorPosition = { x: this.canvasSize.x, y: this.canvasSize.y / 2 };
+        this.cornerAnchorPosition = { x: this.canvasSize.x, y: this.canvasSize.y };
     }
 
     ngAfterViewInit(): void {
@@ -92,6 +99,8 @@ export class DrawingComponent implements AfterViewInit {
     // //Inspired by: https://www.youtube.com/watch?v=NyZSIhzz5Do&ab_channel=JonasGr%C3%B8ndahl
     startResize(event: MouseEvent) {
         this.mouseDown = event.button === MouseButton.Left;
+        this.previouscanvasSize.x += this.canvasSize.x;
+        this.previouscanvasSize.y = this.canvasSize.y;
     }
 
     resize(event: MouseEvent) {
@@ -104,20 +113,43 @@ export class DrawingComponent implements AfterViewInit {
                 this.previewCtx.beginPath();
                 if (this.isBottom) {
                     //Dash horizontal
-                    //starting point from canvas
-                    this.previewCtx.moveTo(canvas.top + window.scrollY, event.pageY);
-                    this.previewCtx.lineTo(canvas.right + window.scrollX, event.pageY);
+                    this.previewCtx.moveTo(
+                        0,
+                        event.pageY - (canvas.top + window.scrollY) >= MIN_HEIGH ? event.pageY - (canvas.top + window.scrollY) : MIN_HEIGH,
+                    );
+                    this.previewCtx.lineTo(
+                        canvas.right + window.scrollX,
+                        event.pageY - (canvas.top + window.scrollY) >= MIN_HEIGH ? event.pageY - (canvas.top + window.scrollY) : MIN_HEIGH,
+                    );
                 } else if (this.isSide) {
                     //dash vertical
-                    this.previewCtx.moveTo(event.pageX - (canvas.left + window.scrollX), canvas.top + window.scrollY);
-                    this.previewCtx.lineTo(event.pageX - (canvas.left + window.scrollX), canvas.bottom + window.scrollY);
+                    this.previewCtx.moveTo(
+                        event.pageX - (canvas.left + window.scrollX) >= MIN_WIDTH ? event.pageX - (canvas.left + window.scrollX) : MIN_WIDTH,
+                        canvas.top + window.scrollY,
+                    );
+                    this.previewCtx.lineTo(
+                        event.pageX - (canvas.left + window.scrollX) >= MIN_WIDTH ? event.pageX - (canvas.left + window.scrollX) : MIN_WIDTH,
+                        canvas.bottom + window.scrollY,
+                    );
                 } else if (this.isCorner) {
                     //dash vertical
-                    this.previewCtx.moveTo(event.pageX - (canvas.left + window.scrollX), canvas.top + window.scrollY);
-                    this.previewCtx.lineTo(event.pageX - (canvas.left + window.scrollX), event.pageY);
+                    this.previewCtx.moveTo(
+                        event.pageX - (canvas.left + window.scrollX) >= MIN_WIDTH ? event.pageX - (canvas.left + window.scrollX) : MIN_WIDTH,
+                        canvas.top + window.scrollY,
+                    );
+                    this.previewCtx.lineTo(
+                        event.pageX - (canvas.left + window.scrollX) >= MIN_WIDTH ? event.pageX - (canvas.left + window.scrollX) : MIN_WIDTH,
+                        event.pageY - (canvas.top + window.scrollY) >= MIN_HEIGH ? event.pageY - (canvas.top + window.scrollY) : MIN_HEIGH,
+                    );
                     //Dash horizontal
-                    this.previewCtx.moveTo(0, event.pageY);
-                    this.previewCtx.lineTo(event.pageX - (canvas.left + window.scrollX), event.pageY);
+                    this.previewCtx.moveTo(
+                        0,
+                        event.pageY - (canvas.top + window.scrollY) >= MIN_HEIGH ? event.pageY - (canvas.top + window.scrollY) : MIN_HEIGH,
+                    );
+                    this.previewCtx.lineTo(
+                        event.pageX - (canvas.left + window.scrollX) >= MIN_WIDTH ? event.pageX - (canvas.left + window.scrollX) : MIN_WIDTH,
+                        event.pageY - (canvas.top + window.scrollY) >= MIN_HEIGH ? event.pageY - (canvas.top + window.scrollY) : MIN_HEIGH,
+                    );
                 }
                 this.previewCtx.stroke();
                 this.previewCtx.setLineDash([]);
@@ -127,18 +159,31 @@ export class DrawingComponent implements AfterViewInit {
 
     stopResize(event: MouseEvent) {
         this.mouseDown = false;
-        const canvas = this.baseCanvas.nativeElement.getBoundingClientRect();
-        console.log(canvas);
+        const canvas = this.previewCanvas.nativeElement.getBoundingClientRect();
+        debugger;
 
         if (this.isBottom) {
-            this.canvasSize.y = event.pageY - (canvas.top + window.scrollY);
+            this.canvasSize.y = event.pageY - (canvas.top + window.scrollY) >= MIN_HEIGH ? event.pageY - (canvas.top + window.scrollY) : MIN_HEIGH;
+            //Relocation of other anchors
+            // this.sideAnchorPosition.y = (event.pageY - (canvas.top + window.scrollY)) / 2;
+            // this.cornerAnchorPosition.y = event.pageY - (canvas.top + window.scrollY);
         } else if (this.isSide) {
-            this.canvasSize.x = event.pageX - (canvas.left + window.scrollX);
+            this.canvasSize.x = event.pageX - (canvas.left + window.scrollX) >= MIN_WIDTH ? event.pageX - (canvas.left + window.scrollX) : MIN_WIDTH;
+            //Relocation of other anchors
+            // this.bottomAnchorPosition.x = (event.pageX - (canvas.left + window.scrollX)) / 2;
+            // this.cornerAnchorPosition.x = event.pageX - (canvas.left + window.scrollX);
         } else if (this.isCorner) {
-            this.canvasSize.x = event.pageX - (canvas.left + window.scrollX);
-            this.canvasSize.y = event.pageY - (canvas.top + window.scrollY);
+            this.canvasSize.x = event.pageX - (canvas.left + window.scrollX) >= MIN_WIDTH ? event.pageX - (canvas.left + window.scrollX) : MIN_WIDTH;
+            this.canvasSize.y = event.pageY - (canvas.top + window.scrollY) >= MIN_HEIGH ? event.pageY - (canvas.top + window.scrollY) : MIN_HEIGH;
+            //Relocation of other anchors
+            //this.bottomAnchorPosition.x = (event.pageX - (canvas.left + window.scrollX)) / 2;
+            //this.bottomAnchorPosition.y = event.pageY - (canvas.top + window.scrollY);
+            //this.bottomAnchorPosition.y = this.canvasSize.y;
+            //this.sideAnchorPosition.x = event.pageX - (canvas.left + window.scrollX);
+            // this.sideAnchorPosition.x = this.canvasSize.x;
+            // this.sideAnchorPosition.y = (event.pageY - (canvas.top + window.scrollY)) / 2;
         }
-
+        //this.relocateAnchors(event, canvas);
         this.mouseDown = false;
         this.isBottom = false;
         this.isCorner = false;
@@ -168,7 +213,6 @@ export class DrawingComponent implements AfterViewInit {
     get height(): number {
         return this.canvasSize.y;
     }
-
     // /* *** Pour une raison quelconque sa fait disparaitre le anchor  quand on depasse
     //  * on depassse la limite de 250x250 ***
     //  */
@@ -181,4 +225,20 @@ export class DrawingComponent implements AfterViewInit {
     //     console.log('verif');
     //     return event.clientY;
     // }
+
+    relocateAnchors(event: MouseEvent, canvas: DOMRect): void {
+        // this.sideAnchorPosition.y = (event.pageY - (canvas.top + window.scrollY)) / 2;
+        // this.sideAnchorPosition.x = event.pageX - (canvas.left + window.scrollX);
+        // this.bottomAnchorPosition.y = event.pageY - (canvas.top + window.scrollY);
+        // this.bottomAnchorPosition.x = (event.pageX - (canvas.left + window.scrollX)) / 2;
+        // this.cornerAnchorPosition.y = event.pageY - (canvas.top + window.scrollY);
+        // this.cornerAnchorPosition.x = event.pageX - (canvas.left + window.scrollX);
+
+        this.sideAnchorPosition.y = this.canvasSize.y / 2;
+        this.sideAnchorPosition.x = this.canvasSize.x;
+        this.bottomAnchorPosition.y = this.previouscanvasSize.y; // + (canvas.top + window.scrollY); // + (canvas.top - window.scrollY); //(canvas.top + window.scrollY);
+        this.bottomAnchorPosition.x = this.canvasSize.x / 2;
+        this.cornerAnchorPosition.y = this.canvasSize.y; //event.pageY - (canvas.top + window.scrollY);
+        this.cornerAnchorPosition.x = this.canvasSize.x; //event.pageX - (canvas.left + window.scrollX);
+    }
 }
