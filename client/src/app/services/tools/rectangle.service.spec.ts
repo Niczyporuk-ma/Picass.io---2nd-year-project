cca49dd14a43b6f5618cd4d9d225c4f10c239402
Yassir <anayass@hotmail.com>
2021-02-14 13:30:16 -0500

    peut etre fin tests rectangle

--- client/src/app/services/tools/rectangle.service.spec.ts
+++ client/src/app/services/tools/rectangle.service.spec.ts
@@ -80,6 +80,19 @@ describe('RectangleService', () => {
         expect(drawLineSpy).not.toHaveBeenCalled();
     });
 
+    it('onMouseUp should not call getPositionFromMouse if shiftIsPressed is true', () =>{
+        const getPositionFromMouseSpy = spyOn(service, 'getPositionFromMouse').and.stub();
+        const mockPath : Vec2[] = [
+            { x: 1, y: 1 },
+            { x: 2, y: 2 },
+        ];
+        service.currentLine = mockPath;
+        service.mouseDown = true;
+        service.shiftIsPressed = true;
+        service.onMouseUp(mouseEvent);
+        expect(getPositionFromMouseSpy).not.toHaveBeenCalled();
+    })
+
     it(' onMouseMove should  call drawLine if shiftIsPresse & moveDown are true and we already have a square', () => {
         service.mouseDown = true;
         service.shiftIsPressed = true;
@@ -143,18 +156,34 @@ describe('RectangleService', () => {
         //expect(service.currentLine).toEqual([{x:1,y:5},{x:5,y:1}]);
     });*/
 
-    it('setShiftIsPressed should have called both drawEllipse and drawRectangle', () => {
+    it('setShiftIsPressed should have called drawLine if checkIfSquare returns false', () => {
         const drawLineSpy = spyOn(service, 'drawLine').and.stub();
 
         const event = new KeyboardEvent('keydown', { key: 'Shift' });
-        service['startingPoint'] = { x: 1, y: 5 };
-        service['endPoint'] = { x: 5, y: 5 };
+        service.startingPoint = { x: 1, y: 5 };
+        service.endPoint = { x: 5, y: 5 };
 
         service.setShiftIsPressed(event);
         expect(drawLineSpy).toHaveBeenCalled();
     });
 
-    
+    it('setShiftIsPressed shouldnt call drawLine if checkIfSquare returns true', () => {
+        const drawLineSpy = spyOn(service, 'drawLine').and.stub();
+
+        const event = new KeyboardEvent('keydown', { key: 'Shift' });
+        service.startingPoint = { x: 1, y: 1 };
+        service.endPoint = { x: 5, y: 5 };
+
+        service.setShiftIsPressed(event);
+        expect(drawLineSpy).not.toHaveBeenCalled();
+    });
+
+    it('setShiftIsPressed does nothing if the key isnt shift', () => {
+        service.shiftIsPressed = false;
+        const event = new KeyboardEvent('keydown', { key: 'A' });
+        service.setShiftIsPressed(event);
+        expect(service.shiftIsPressed).toBeFalse();
+    })
 
     it('setShiftNonPressed sets shifts shiftIsPressed and eventTest to false when mouseDown is true', () => {
         service.mouseDown = true;
@@ -166,18 +195,39 @@ describe('RectangleService', () => {
         expect(service.eventListenerIsSet).toEqual(false);
     });
 
-    it('setShiftNonPressed sets shifts shiftIsPressed to false when mouseDown is false', () => {
+    it('setShiftNonPressed sets shiftIsPressed to false when mouseDown is false', () => {
         service.mouseDown = false;
         const event = new KeyboardEvent('keydown', { key: 'Shift' });
         service.setShiftNonPressed(event);
         expect(service.shiftIsPressed).toEqual(false);
     });
 
+    it('setShiftNonPressed does nothing if key isnt shift', () => {
+        service.shiftIsPressed = true;
+        const event = new KeyboardEvent('keydown', { key: 'A' });
+        service.setShiftNonPressed(event);
+        expect(service.shiftIsPressed).toBeTrue();
+    })
+
     it('onShift returns eventTest true', () => {
         service.eventListenerIsSet = false;
         service.onShift();
         expect(service.eventListenerIsSet).toEqual(true);
     });
+
+    it('onShift adds 2 eventlisteners if eventListenerIsSet is false', () =>{
+        const eventListenerSpy = spyOn(window,'addEventListener').and.stub();
+        service.eventListenerIsSet = false;
+        service.onShift();
+        expect(eventListenerSpy).toHaveBeenCalledTimes(2);
+    })
+
+    it('onShift does nothing if eventListenerIsSet is true', () =>{
+        const eventListenerSpy = spyOn(window,'addEventListener').and.stub();
+        service.eventListenerIsSet = true;
+        service.onShift();
+        expect(eventListenerSpy).not.toHaveBeenCalled();
+    })
 
     it('drawLine should calls fillRect when toolStyle.fill is true', () => {
         const rectangleSpyObject = jasmine.createSpyObj<CanvasRenderingContext2D>('CanvasRenderingContext2D', [
