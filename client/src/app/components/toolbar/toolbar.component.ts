d0b773f46b7cad489e86979e62fe8fa520575efb
Slimane <slimaneb2000@gmail.com>
2021-02-15 15:26:48 -0500

    more QA

--- client/src/app/components/toolbar/toolbar.component.ts
+++ client/src/app/components/toolbar/toolbar.component.ts
@@ -31,11 +31,11 @@ export class ToolbarComponent {
         this.tools = toolManager.tools;
     }
 
-    setRectangleStyle(n: string): void {
-        if (n === FILL_VALUE) {
+    setRectangleStyle(recStyleCode: string): void {
+        if (recStyleCode === FILL_VALUE) {
             this.toolManager.rectangleService.toolStyles.fill = true;
             this.toolManager.rectangleService.contour = false;
-        } else if (n === CONTOUR_VALUE) {
+        } else if (recStyleCode === CONTOUR_VALUE) {
             this.toolManager.rectangleService.toolStyles.fill = false;
             this.toolManager.rectangleService.contour = true;
         } else {
@@ -48,11 +48,11 @@ export class ToolbarComponent {
         this.showPalette = !this.showPalette;
     }
 
-    setEllipseStyle(n: string): void {
-        if (n === FILL_VALUE) {
+    setEllipseStyle(ellipseStyleCode: string): void {
+        if (ellipseStyleCode === FILL_VALUE) {
             this.toolManager.ellipseService.toolStyles.fill = true;
             this.toolManager.ellipseService.border = false;
-        } else if (n === CONTOUR_VALUE) {
+        } else if (ellipseStyleCode === CONTOUR_VALUE) {
             this.toolManager.ellipseService.toolStyles.fill = false;
             this.toolManager.ellipseService.border = true;
         } else {
@@ -73,8 +73,8 @@ export class ToolbarComponent {
         }
     }
 
-    changeDiameter(n: number): void {
-        this.toolManager.lineService.currentDiameter = n;
+    changeDiameter(newDiameter: number): void {
+        this.toolManager.lineService.currentDiameter = newDiameter;
     }
 
     changeWidth(width: number): void {
