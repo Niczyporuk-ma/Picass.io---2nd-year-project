1a8258333c2618dbd109953591bf922539f3160f
Yassir <anayass@hotmail.com>
2021-02-15 22:21:19 -0500

    tests

--- client/src/app/services/tools/color.service.spec.ts
+++ client/src/app/services/tools/color.service.spec.ts
@@ -15,25 +15,25 @@ fdescribe('ColorService', () => {
         expect(service).toBeTruthy();
     });
 
-    // it('setPrimaryColorWithOpacity updates the opacity of the primary color', () => {
-    //     service.primaryColor = 'rgba(255,255,255,1)';
-    //     service.setPrimaryColorWithOpacity(0.5);
-    //     expect(service.primaryColor).toEqual('rgba(255,255,255,0.5)');
-    // });
+    it('setPrimaryColorWithOpacity updates the opacity of the primary color', () => {
+        service.primaryColorPreview = 'rgba(255,255,255,1)';
+        service.setPrimaryColorWithOpacity(0.5);
+        expect(service.primaryColorPreview).toEqual('rgba(255,255,255,0.5)');
+    });
 
-    // it('setSecondary ColorWithOpacity updates the opacity of the secondary color', () => {
-    //     service.secondaryColor = 'rgba(255,255,255,1)';
-    //     service.setSecondaryColorWithOpacity(0.5);
-    //     expect(service.secondaryColor).toEqual('rgba(255,255,255,0.5)');
-    // });
+    it('setSecondary ColorWithOpacity updates the opacity of the secondary color', () => {
+        service.secondaryColorPreview = 'rgba(255,255,255,1)';
+        service.setSecondaryColorWithOpacity(0.5);
+        expect(service.secondaryColorPreview).toEqual('rgba(255,255,255,0.5)');
+    });
 
-    // it('swapPrimaryAndSecondary inverts the primary and the secondary colors', () => {
-    //     service.primaryColor = 'rgba(0,0,0,1)';
-    //     service.secondaryColor = 'rgba(255,255,255,1)';
-    //     service.swapPrimaryAndSecondary();
-    //     expect(service.primaryColor).toEqual('rgba(255,255,255,1)');
-    //     expect(service.secondaryColor).toEqual('rgba(0,0,0,1)');
-    // });
+    it('swapPrimaryAndSecondary inverts the primary and the secondary colors', () => {
+        service.primaryColor = 'rgba(0,0,0,1)';
+        service.secondaryColor = 'rgba(255,255,255,1)';
+        service.swapPrimaryAndSecondary();
+        expect(service.primaryColor).toEqual('rgba(255,255,255,1)');
+        expect(service.secondaryColor).toEqual('rgba(0,0,0,1)');
+    });
 
     it('setColorWithOpacity should return the right string with color and opacity', () =>{
         const mockColor : string = 'rgba(1,2,3,4)';
