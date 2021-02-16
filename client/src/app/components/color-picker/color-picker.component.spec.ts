010fbc5fd656e50fee1b5cf8fc376d361f4aa908
Yassir <anayass@hotmail.com>
2021-02-16 13:41:15 -0500

    remise

--- client/src/app/components/color-picker/color-picker.component.spec.ts
+++ client/src/app/components/color-picker/color-picker.component.spec.ts
@@ -3,6 +3,7 @@ import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { ColorPickerComponent } from './color-picker.component';
 
+
 describe('ColorPickerComponent', () => {
     let component: ColorPickerComponent;
     let fixture: ComponentFixture<ColorPickerComponent>;
