fb38565c3246f928b30e1c9ed22ba58be973bc6f
Kevin Lam <kevinlam@Kevins-MacBook-Pro.local>
2021-02-21 21:32:12 -0500

    style du polygone implemente

--- client/src/app/services/tools/polygon.service.ts
+++ client/src/app/services/tools/polygon.service.ts
@@ -6,79 +6,72 @@ import { DrawingService } from '@app/services/drawing/drawing.service';
 import { ColorService } from '@app/services/tools/color.service';
 import { SquareHelperService } from '@app/services/tools/square-helper.service';
 
-
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root',
 })
-export class PolygonService extends Tool{
+export class PolygonService extends Tool {
+    startingPoint: Vec2;
+    endPoint: Vec2;
+    currentLine: Vec2[] = [];
+    contour: boolean = true;
+    sides: number = 3;
+    premNumberOfSides: number = 3;
+    showNumberOfSidesInput: boolean = false;
+    centerX: number;
+    centerY: number;
+    angle: number;
+    radius: number;
+    squareCornerPos: Vec2;
 
-      startingPoint: Vec2;
-      endPoint: Vec2;
-      currentLine: Vec2[] = [];
-      contour: boolean = true;
-      sides : number = 3;
-      premNumberOfSides : number = 3;
-      showNumberOfSidesInput : boolean = false;
-      centerX : number;
-      centerY : number;
-      angle : number;
-      radius : number;
-      squareCornerPos : Vec2;
-
-      constructor(drawingService: DrawingService, public colorService: ColorService, private squareHelper : SquareHelperService) 
-      {
+    constructor(drawingService: DrawingService, public colorService: ColorService, private squareHelper: SquareHelperService) {
         super(drawingService);
         this.shortcut = '3';
-        this.localShortcuts = new Map ();
+        this.localShortcuts = new Map();
         this.currentLine = [];
         this.index = 5;
         this.toolStyles = {
-          primaryColor: 'rgba(255, 0, 0, 1)',
-          lineWidth: 1,
-          fill: false,
-          secondaryColor: 'black',
+            primaryColor: 'rgba(255, 0, 0, 1)',
+            lineWidth: 1,
+            fill: false,
+            secondaryColor: 'black',
         };
-
-      }
+    }
 
     clearArrays(): void {
         this.currentLine = [];
     }
 
-    setPreliminaryNumberOfSides(event : KeyboardEvent): void 
-    {
-      this.premNumberOfSides = parseInt((event.target as HTMLInputElement).value);
+    setPreliminaryNumberOfSides(event: KeyboardEvent): void {
+        this.premNumberOfSides = parseInt((event.target as HTMLInputElement).value);
     }
 
-    setNumberOfSides() : void 
-    {
-        if(this.premNumberOfSides >= 3 && this.premNumberOfSides <= 12){
-          this.sides = this.premNumberOfSides;
-        }else
-        {
-            alert("Veuillez inserer un nombre entre 3 et 12");
+    setNumberOfSides(): void {
+        if (this.premNumberOfSides >= 3 && this.premNumberOfSides <= 12) {
+            this.sides = this.premNumberOfSides;
+        } else {
+            alert('Veuillez inserer un nombre entre 3 et 12');
         }
     }
 
-      onMouseDown(mouseDownEvent: MouseEvent): void {
+    onMouseDown(mouseDownEvent: MouseEvent): void {
         this.mouseDown = mouseDownEvent.button === MouseButton.Left;
         if (this.mouseDown) {
             this.mouseDownCoord = this.getPositionFromMouse(mouseDownEvent);
             this.startingPoint = this.mouseDownCoord;
         }
-      }
+    }
 
-      onMouseUp(mouseUpEvent: MouseEvent): void {
+    onMouseUp(mouseUpEvent: MouseEvent): void {
         if (this.mouseDown && !this.drawingService.resizeActive) {
-          const mousePosition = this.getPositionFromMouse(mouseUpEvent);
-          this.endPoint = mousePosition;
-          this.drawingService.clearCanvas(this.drawingService.previewCtx);
-          this.drawLine(this.drawingService.baseCtx, this.currentLine);
+            const mousePosition = this.getPositionFromMouse(mouseUpEvent);
+            this.endPoint = mousePosition;
+            this.drawingService.clearCanvas(this.drawingService.previewCtx);
+            this.drawLine(this.drawingService.baseCtx, this.currentLine);
         }
         this.mouseDown = false;
-      }
+    }
 
-      onMouseMove(mouseMoveEvent: MouseEvent): void {
+    onMouseMove(mouseMoveEvent: MouseEvent): void {
         if (this.mouseDown && !this.drawingService.resizeActive) {
             const mousePosition = this.getPositionFromMouse(mouseMoveEvent);
             this.endPoint = mousePosition;
@@ -86,18 +79,15 @@ export class PolygonService extends Tool{
             this.drawingService.clearCanvas(this.drawingService.previewCtx);
             this.computeCircleValues(this.currentLine);
             this.drawLine(this.drawingService.previewCtx, this.currentLine);
-            this.drawCircle(this.drawingService.previewCtx,this.currentLine);
+            this.drawCircle(this.drawingService.previewCtx, this.currentLine);
         }
-      }
+    }
 
-      onButtonPress(): void 
-      {
-        this.showNumberOfSidesInput = !(this.showNumberOfSidesInput);
-      }
+    onButtonPress(): void {
+        this.showNumberOfSidesInput = !this.showNumberOfSidesInput;
+    }
 
-
-
-      drawLine(ctx: CanvasRenderingContext2D, path: Vec2[]): void {
+    drawLine(ctx: CanvasRenderingContext2D, path: Vec2[]): void {
         //partie couleur
         this.setColors(this.colorService);
         this.setStyles();
@@ -122,28 +112,25 @@ export class PolygonService extends Tool{
             this.drawingService.drawingStarted = true;
         }
 
-       //this.computeCircleValues(path);
+        //this.computeCircleValues(path);
 
         ctx.beginPath();
         ctx.setLineDash([]);
 
-        ctx.moveTo (this.centerX +  this.radius * Math.cos(0), this.centerY);  // pour le mettre sur l'extremité du cercle
+        ctx.moveTo(this.centerX + this.radius * Math.cos(0), this.centerY); // pour le mettre sur l'extremité du cercle
 
-        for(let i = 1; i <= this.sides;i++ )
-        {
-          ctx.lineTo (this.centerX + this.radius * Math.cos(i * this.angle), this.centerY + this.radius * Math.sin(i * this.angle));
+        for (let i = 1; i <= this.sides; i++) {
+            ctx.lineTo(this.centerX + this.radius * Math.cos(i * this.angle), this.centerY + this.radius * Math.sin(i * this.angle));
         }
-       
+
         ctx.stroke();
-        
+
         if (this.toolStyles.fill) {
-           ctx.setLineDash([]);
             ctx.fill();
         }
-      }
+    }
 
-      drawCircle(ctx: CanvasRenderingContext2D, path: Vec2[])
-      {
+    drawCircle(ctx: CanvasRenderingContext2D, path: Vec2[]) {
         const gapBetweenDash = 5;
         const dashLength = 5;
         ctx.beginPath();
@@ -151,19 +138,16 @@ export class PolygonService extends Tool{
         ctx.strokeStyle = 'black';
         ctx.globalCompositeOperation = 'source-over';
         ctx.setLineDash([dashLength, gapBetweenDash]);
-        ctx.arc(this.centerX,this.centerY,this.radius,0,2 * Math.PI,);
+        ctx.arc(this.centerX, this.centerY, this.radius, 0, 2 * Math.PI);
         ctx.stroke();
         ctx.setLineDash([]);
-      }
-      
-      computeCircleValues(path: Vec2[]):void
-      {
-        this.squareCornerPos = this.squareHelper.closestSquare([path[0], path[1]]);
-        this.centerX  = (path[0].x + this.squareCornerPos.x)/2;
-        this.centerY = (path[0].y + this.squareCornerPos.y)/2;
-        this.angle = ((Math.PI*2)/this.sides);
-        this.radius  = Math.abs((path[0].x - this.squareCornerPos.x) / 2);
-      }
+    }
 
+    computeCircleValues(path: Vec2[]): void {
+        this.squareCornerPos = this.squareHelper.closestSquare([path[0], path[1]]);
+        this.centerX = (path[0].x + this.squareCornerPos.x) / 2;
+        this.centerY = (path[0].y + this.squareCornerPos.y) / 2;
+        this.angle = (Math.PI * 2) / this.sides;
+        this.radius = Math.abs((path[0].x - this.squareCornerPos.x) / 2);
+    }
 }
- 
